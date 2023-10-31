61f49298878d97d4b8a04c3214659db3fb008a0f
Joao Victor <lealjoaovictor2004@gmail.com>
2023-10-30 21:31:55 -0300

    Sistema de validação de Usuários

--- Model/Usuario.py
+++ Model/Usuario.py
@@ -1,7 +1,52 @@
 class Usuario:
     def __init__(self):
-        self.codigo = None
-        self.nome = None
-        self.tipo = None
-        self.login = None
-        self.senha = None
+        self.__codigo = None
+        self.__nome = None
+        self.__tipo = None
+        self.__login = None
+        self.__senha = None
+        self.__usuarios = {}
+
+    def get_codigo(self):
+        return self.__codigo
+
+    def get_nome(self):
+        return self.__nome
+
+    def get_tipo(self):
+        return self.__tipo
+
+    def get_login(self):
+        return self.__login
+
+    def get_senha(self):
+        return self.__senha
+
+    def set_login(self, login):
+        self.__login = login
+
+    def set_senha(self, senha):
+        self.__senha = senha
+
+    def getUsuarios(self):
+        return self.__usuarios
+
+    def instanciar_usr(self, codigo, nome, tipo):
+        self.__codigo = codigo
+        self.__nome = nome
+        self.__tipo = tipo
+
+    def validar_acesso(self):
+        # Lê o arquivo de usuários e armazena as informações em um dicionário
+        with open('txt/usuarios.txt', 'r') as arquivo_usuarios:
+            for linha in arquivo_usuarios:
+                codigo, nome, tipo, login, senha = linha.strip().split(',')
+                self.__usuarios[login] = {'codigo': codigo, 'nome': nome, 'tipo': tipo, 'senha': senha}
+                if self.__login == login and self.__senha == senha:
+                    codigo = self.__usuarios[login]['codigo']
+                    nome = self.__usuarios[login]['nome']
+                    tipo = self.__usuarios[login]['tipo']
+                    self.instanciar_usr(codigo, nome, tipo)
+                    return True
+        return False
+
--- main.py
+++ main.py
@@ -1,22 +1,12 @@
-def validar_acesso():
-    usuarios = {}
-
-    # Lê o arquivo de usuários e armazena as informações em um dicionário
-    with open('txt/usuarios.txt', 'r') as arquivo_usuarios:
-        for linha in arquivo_usuarios:
-            codigo, nome, tipo, login, senha = linha.strip().split(',')
-            usuarios[login] = {'codigo': codigo, 'nome': nome, 'tipo': tipo, 'senha': senha}
-
-    login = input("Login: ")
-    senha = input("Senha: ")
-
-    if login in usuarios and usuarios[login]['senha'] == senha:
-        nome_do_usuario = usuarios[login]['nome']
-        print(f"Bem-vindo(a), {nome_do_usuario}!")
-
-    else:
-        print("Acesso negado. Credenciais inválidas.")
+from Model.Usuario import Usuario
 
 
 if __name__ == '__main__':
-    validar_acesso()
+    usuario = Usuario()
+    usuario.set_login(input("Login: "))
+    usuario.set_senha(input("Senha: "))
+
+    if usuario.validar_acesso():
+        print(f"Acesso permitido! Bem vindo(a) {usuario.get_nome()}.")
+    else:
+        print("Acesso Negado!")
